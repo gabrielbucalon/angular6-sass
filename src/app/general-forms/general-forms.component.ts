c23b06361bfadcb17b80daf7340cb6fb39946e00
Gabriel <bucalongabriell@gmail.com>
2018-09-26 15:06:50 -0300

    Feature - Ajustando responsivo

--- src/app/general-forms/general-forms.component.ts
+++ src/app/general-forms/general-forms.component.ts
@@ -7,6 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GeneralFormsComponent implements OnInit {
 
+  brandPhotoWorker = ' Foto do trabalhador';
+  brandTitlePhoto = 'Anexar Foto';
+  brandTitlePersonalData = ' Dados pessoais';
+  attachDocument = 'Anexar Documento';
+  outherFile = {field: 'Outro Arquivo'};
+  brandEndress = 'Endereço';
+
+  sendFiles = [
+    {name: 'Exame Medico'},
+    {name: 'Carteira de trabalho'},
+    {name: 'CNH'},
+    {name: 'Comprovante de endereço'},
+  ];
+
+  endress = [
+    {name: 'CEP'},
+    {name: 'Endereço'},
+    {name: 'Complemento'},
+    {name: 'Bairro'},
+    {name: 'Estado'},
+    {name: 'Cidade'},
+  ];
+
+  personalData = [
+    {name: 'Matricula'},
+    {name: 'Nome'},
+    {name: 'N° Pis *'},
+    {name: 'CPF'},
+    {name: 'RG'},
+    {name: 'N° CTPS'},
+    {name: 'Data Nascimento'},
+    {name: 'Data Admissão'},
+    {name: 'Data Admissão'},
+    {name: 'Mãe'},
+    {name: 'Pai'},
+  ];
   constructor() { }
 
   ngOnInit() {
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
   brandName = 'Teste';
 
-  profile = {name: 'Jhon Davis', user: 'Jhon123456', email: 'jhon@gmail.com'};
+  profile = { user: 'Perfil', logout: 'Sair'};
 
   constructor() { }
 
